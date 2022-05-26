5cdf74feacbcaeff278c79940e16074da6448261
Andrew Gibbons <andrew.h.gibbons@gmail.com>
2022-05-26 18:57:21 +0900

    Added support for different key lengths

--- app/Main.hs
+++ app/Main.hs
@@ -15,14 +15,6 @@ import Rjindael
 import Types
 import Block
 
---import Criterion.Main
-
--- Ideal usage
--- ecbEncrypt key plaintext\
-
-{-main :: IO ()
-main = putStrLn "Hello"-}
-
 main :: IO ()
 main = runEncrypter =<< execParser opts
   where
@@ -31,27 +23,17 @@ main = runEncrypter =<< execParser opts
      <> progDesc "Encrypt a file using AES (ECB or CBC mode)"
      <> header "AES Encrypt" )
 
-{-data Arguments = Arguments {infile_ :: FilePath,
-                            encryptFlag_ :: Bool,
-                            outfile_ :: FilePath,
-                            blockmode_ :: String,
-                            keyfile_  :: FilePath,
-                            ivfile_   :: FilePath
-                            } deriving (Show, Eq)-}
-
-
 
 data AESArguments = AESArguments 
                 {infile_     :: FilePath,
                 outfile_     :: FilePath,
                 encryptFlag_ :: EncDec,
                 blockmode_   :: String,
+                keyfile_     :: FilePath,
                 ivfile_      :: FilePath,
-                keyfile_     :: FilePath
+                keylen_      :: String
                 } deriving (Show, Eq)
 
--- ModeParser = ECBMode | CBCMode ivfile
-data BlockMode = ECBMode | CBCMode FilePath deriving (Show, Eq)
 
 data EncDec = Encrypt | Decrypt deriving (Show, Eq)
 
@@ -82,6 +64,11 @@ mainparser = AESArguments
          <> metavar "IV"
          <> value ""
          <> help "Path to IV value.")
+      <*> strOption
+          ( long "keylen"
+         <> metavar "KEYLEN"
+         <> value "128"
+         <> help "AES key length (128 (default) or 192 or 256)")
 
 encrypter :: AES -> Key -> PlainText -> String 
           -> Maybe IV -> Either String CipherText 
@@ -105,32 +92,20 @@ runEncrypter args = do
 
     key    <- BS.readFile $ keyfile_ args
 
-    if (mode == "cbc") && (ivfile_ args == "") then error "No IV file."
-                                               else return ()
     iv     <- if mode=="cbc" then (fmap Just . BS.readFile $ ivfile_ args)
                           else (return Nothing)
     intext <- BS.readFile $ infile_ args
+    let keylen = case keylen_ args of
+                    "128" -> AES128
+                    "192" -> AES192
+                    "256" -> AES256
+                    _     -> error "Invalid key length." 
+
 
     let otext = case encf of 
-                    Decrypt -> decrypter AES128 key iv intext mode 
-                    Encrypt -> encrypter AES128 key intext mode iv
-
-    BS.writeFile ofile (fromRight "" otext)
-    
-
-
-{-main = defaultMain [
-           bgroup "rotate" [ bench "abcdefg"  $ nf (rotateList 5) "abcdefg"
-               , bench "4rf8uhf36gr"  $ nf (rotateList 3) "4rf8uhf36gr"
-               , bench "jn6789okj4567654egh8uhgftytr"  $ nf (rotateList 10) "jn6789okj4567654egh8uhgftytr"
-               , bench "long"  $ nf (rotateList 8) "a6yg4567tgsghutjhtgfbhyhtgre57ytrsgrthy78u76ytgarsevrydtuk7iuyrrgghts6uthnyjkyt"
-               ],
-           bgroup "rotate2" [ bench "abcdefg"  $ nf (rotateList2 5) "abcdefg"
-               , bench "4rf8uhf36gr"  $ nf (rotateList2 3) "4rf8uhf36gr"
-               , bench "jn6789okj4567654egh8uhgftytr"  $ nf (rotateList2 10) "jn6789okj4567654egh8uhgftytr"
-               , bench "Long"  $ nf (rotateList2 8) "a6yg4567tgsghutjhtgfbhyhtgre57ytrsgrthy78u76ytgarsevrydtuk7iuyrrgghts6uthnyjkyt"
-               ]
-            
-  ]-}
-
-
+                    Decrypt -> decrypter keylen key iv intext mode 
+                    Encrypt -> encrypter keylen key intext mode iv
+    case otext of
+        Left s -> do putStrLn "Error!"
+                     putStrLn s
+        Right a -> BS.writeFile ofile a 
